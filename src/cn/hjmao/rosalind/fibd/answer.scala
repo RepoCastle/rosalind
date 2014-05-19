2a37f1f4b69f8c518a638f64e3a5dd04a2427ac6
Huajian Mao <huajianmao@gmail.com>
2014-05-19 09:00:49 +0800

    Mortal Fibonacci Rabbits @ problems/fib

--- src/cn/hjmao/rosalind/fibd/answer.scala
+++ src/cn/hjmao/rosalind/fibd/answer.scala
@@ -2,40 +2,25 @@ package cn.hjmao.rosalind.fibd
 
 object Main extends App {
 
-  /*
-  def fibd(n: Int, m: Int): Long = {
-    n match {
-      case 0 => 1
-      case 1 => 1
-      case 2 => 1
-      case i if i<=m => fibd(n-1, m) + fibd(n-2, m)
-      case _ => fibd(n-1, m) + fibd(n-2, m) - fibd(n-m-1, m)
-    }
-  }
-  */
-
-  def fibd(n: Int, m:Int, table: Map[Int, Long]): Map[Int, Long] = {
+  def fibd(n: Int, m:Int, i: Int, table: Map[Int, BigInt]): Map[Int, BigInt] = {
     table.contains(n) match {
       case true => table
       case false => {
-        val n_1th = table(n-1)
-        val n_2th = table(n-2)
+        val i_1th = table(i-1)
+        val i_2th = table(i-2)
 
-        val nth = n match {
-          case i if i<=m => n_1th + n_2th
-          case _ => val m_prevth = table(n-m-1); n_1th + n_2th - m_prevth
+        val ith = i match {
+          case j if j<=m => i_1th + i_2th
+          case _ => i_1th + i_2th - table(i-m-1)
         }
 
-        fibd(n, m,table.filter(_._1 != n-m-1) + ((n, nth)))
+        fibd(n, m, i+1, table.filter(_._1 > i-m-1) + ((i, ith)))
       }
     }
   }
-
   
   val nm = scala.io.Source.fromFile("dataset.txt").mkString.trim().split("\\s+")
-  // val n = nm(0).toInt
-  // val m = nm(1).toInt
-  val n = 3
-  val m = 3
-  println(fibd(n, m, Map(1->1, 2->1)).getOrElse(n, 0))
+  val n = nm(0).toInt
+  val m = nm(1).toInt
+  println(fibd(n, m, 3, Map(0->1, 1->1, 2->1))(n))
 }
